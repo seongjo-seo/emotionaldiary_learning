4d9e5548b2ae25349b48d342c1f2c13d0ed84386
seoseongjo <tjtjdwh5321@gmail.com>
2022-08-31 23:59:25 +0900

    onChange 값 변경 학습

--- diary/src/DiaryEditor.js
+++ diary/src/DiaryEditor.js
@@ -1,13 +1,18 @@
 import {useState} from 'react';
 
 const DiaryEditor = () =>{
-  const [author, setAuthor] =useState("");
+  const [author, setAuthor] =useState("서성조");
 
   return(
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input value={author}/>
+        <input
+        value={author}
+        onChange={(e)=>{
+        console.log(e);
+        }}
+        />
       </div>
     </div>
   );
