70c719a17d04f2b240aec7000341a0899be60bd2
seoseongjo <tjtjdwh5321@gmail.com>
2022-09-02 23:51:26 +0900

    useState 값 전달

--- diary/src/DiaryEditor.js
+++ diary/src/DiaryEditor.js
@@ -1,17 +1,27 @@
 import {useState} from 'react';
 
 const DiaryEditor = () =>{
-  const [author, setAuthor] =useState("서성조");
+  const [author, setAuthor] =useState("");
+  const [content, setContent] =useState("");
 
   return(
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+        name ="author"
         value={author}
         onChange={(e)=>{
-        console.log(e);
+          setAuthor(e.target.value);
         }}
+        />
+        </div>
+        <div>
+        <textarea
+          value ={content}
+          onChange = {(e) => {
+            setContent(e.target.value);
+          }}
         />
       </div>
     </div>
