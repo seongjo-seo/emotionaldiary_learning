08eb04fedffdea7cd6389c19d690fbb06ea357d9
seoseongjo <tjtjdwh5321@gmail.com>
2022-10-16 23:55:48 +0900

    input value set

--- diary/src/DiaryEditor.js
+++ diary/src/DiaryEditor.js
@@ -2,69 +2,17 @@ import {useState} from 'react';
 
 const DiaryEditor = () =>{
 
-  const authorInput = useRef();
-  const contentInput = useRef();
+  const [author, setAuthor] = useState("");
 
-  const [state, setState] =useState({
-    author:"",
-    content:"",
-    emotion:1,
-  });
-
-  const handleChangeState = (e) =>{
-    console.log(e.target.name);
-    console.log(e.target.value);
-
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
-  }
-  const handleSubmit = () =>{
-    if(state.author.length<1){
-      authorInput.current.focus();
-      return ;
-    }
-    if(state.content.length <5){
-      contentInput.current.focus();
-      return ;
-    }
-    alert("저장 성공");
-  }
-
-  return(
-    <div className="DiaryEditor">
+  return (
+    <div className='DiaryEditor'>
       <h2>오늘의 일기</h2>
       <div>
-        <input
-        ref={authorInput}
-        name ="author"
-        value={state.author}
-        onChange={handleChangeState}
-        />
-        </div>
-        <div>
-        <textarea
-        ref={contentInput}
-          name ="content"
-          value ={state.content}
-          onChange = {handleChangeState}
-        />
-      </div>
-      <div>
-        <select
-        name="emontion"
-        value={state.emotion}
-        onChange={handleChangeState}>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-        </select>
-      </div>
-      <div>
-        <button onClick={handleSubmit}>일기 저장하기.</button>
+        <input value={author}
+        onChange={(e)=>{
+          console.log(e.target.value);
+        }}
+          />
       </div>
     </div>
   );
