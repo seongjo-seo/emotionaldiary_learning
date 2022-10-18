822cd81ae807cff4258ee4a12a4841db7406f57e
seongjo-seo <tjtjdwh5321@gmail.com>
2022-10-18 09:20:57 +0900

    List, Item 생성 및 map 함수를 활용하여 List Data 전달 받는 방법 학습

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
-function App() {
+const dummyList = [
+  {
+    id: 1,
+    author: "서성조",
+    content:"하이 1",
+    emotion:5,
+    /** 밀리초를 받아서 변경하려고 한다. */
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content:"하이 2",
+    emotion:2,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "김치만",
+    content:"하이 3",
+    emotion:1,
+    created_date: new Date().getTime()
+  },
+]
+
+const App = () => {
   return (
     <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
---
+++ diary/src/DiaryList.js
@@ -0,0 +1,22 @@
+import DiaryItem from './DiaryItem'
+
+const DiaryList = ({diaryList})=>{
+	return (
+		<div className ="DiaryList">
+			<h2>일기 리스트</h2>
+			<h4>{diaryList.length}개의 일기가 있습니다.</h4>
+			<div>
+				{diaryList.map((it)=>(
+					<DiaryItem key={it.id} {...it}/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+/** 더미 데이터가 없는 경우 빈 배열로 초기화 하는 방법 */
+DiaryList.defaultProps ={
+	diaryList: [],
+}
+
+export default DiaryList;
