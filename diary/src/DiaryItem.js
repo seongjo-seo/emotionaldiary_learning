35ba6f7a1c362af1b9a8fd2ce6f8bd11d5eda5d8
seongjo-seo <tjtjdwh5321@gmail.com>
2022-10-19 10:26:56 +0900

    data delete 학습 및 코드 적용

--- diary/src/DiaryItem.js
+++ diary/src/DiaryItem.js
@@ -1,13 +1,21 @@
-const DiaryItem = ({author, content, created_date, emotion, id}) =>{
-    return <div className ="DiaryItem">
-        <div className="info">
-            <span>
-                작성자 : {author} | 감정점수 : {emotion}
-            </span>
-            <span className ="date"> {new Date(created_date).toLocaleDateString()}</span>
-        </div>
-        <div className="content">{content}</div>
-    </div>;
+const DiaryItem = ({onDelete, author, content, created_date, emotion, id}) =>{
+	return <div className ="DiaryItem">
+		<div className="info">
+			<span>
+					작성자 : {author} | 감정 : {emotion}
+			</span>
+			<span className ="date"> {new Date(created_date).toLocaleDateString()}</span>
+		</div>
+		<div className="content">{content}</div>
+		<button
+			onClick={()=>{
+				if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
+					onDelete(id);
+				}
+			}}>
+			삭제하기
+		</button>
+	</div>;
 };
 
 export default DiaryItem;
