6a3c4ebe4ab82821fbcf83bb66f8044e3140207f
seongjo-seo <tjtjdwh5321@gmail.com>
2022-10-19 10:15:18 +0900

    data insert 학습 및 적용

--- diary/src/DiaryEditor.js
+++ diary/src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import {useRef , useState} from 'react';
 
-const DiaryEditor = () =>{
+const DiaryEditor = ({onCreate}) =>{
   
   const authorInput = useRef();
   const contentInput = useRef();
@@ -28,8 +28,14 @@ const DiaryEditor = () =>{
       contentInput.current.focus();
       return;
     }
-
+    
+    onCreate(state.author, state.content, state.emotion);
     alert("저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
   };
 
 
