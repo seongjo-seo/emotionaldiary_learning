a290fa1cb4f13ba2b84b48b8949ecefa00247318
seongjo-seo <tjtjdwh5321@gmail.com>
2022-10-21 17:58:16 +0900

    API 활용 및 useEffect 학습용 js 제거

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,13 +1,38 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+
+/** https://jsonplaceholder.typicode.com/comments을 통해서 API 값 호출하여 활용 */
 
 const App = () => {
 
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async (author, content, emotion)=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=>res.json())
+    console.log(res);
+
+    const initData = res.slice(0,20).map((it)=>{
+      return {
+        author : it.email,
+        content : it.body,
+        emotion : Math.floor(Math.random() * 5)+1,
+        created_date : new Date().getTime(),
+        id : dataId.current++
+
+      };
+
+    });
+
+    setData(initData)
+  }
+
+  useEffect(()=>{
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) =>{
     const created_date = new Date().getTime();
