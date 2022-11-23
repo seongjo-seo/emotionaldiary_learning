00a867a590fb8a265983c431e07fb387713368de
seoseongjo <tjtjdwh5321@gmail.com>
2022-11-23 09:19:53 +0900

    page에 useParams, useNavigate, Query String 확인 및 복습

--- diary/src/pages/Diary.js
+++ diary/src/pages/Diary.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Diary = () => {
+  /**
+   * useParams은 id 값을 갖고올 때
+   * 어떤 일기를 보여줘야 할 지 전달 받아서 사용할 경우 적용.
+   */
+  const {id} = useParams();
+  console.log(id);
+
   return (
     <>
     <h1>Diary</h1>
--- diary/src/pages/Edit.js
+++ diary/src/pages/Edit.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const Edit = () => {
+  /**
+   * useNavigate 함수는 페이지를 이동시켜주는 함수를 갖고 있다.
+   */
+  const navigate = useNavigate();
+
+  /**
+   * Query String
+   * 웹 페이지에 데이터를 전달하는 방법 중 가장 간단한 방법이다.
+   * 실시간으로 변경될 수 있는 setSearchParams 상태 변화 함수를 갖고 있다.
+   */
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
   return (
     <>
     <h1>Edit</h1>
     <p>이곳은 일기 수정 페이지 입니다.</p>
+    <button onClick={()=> setSearchParams({who : "winterlood"})}>
+      QS 바꾸기
+    </button>
+
+    <button onClick={()=>{
+      navigate("/home");
+    }}>
+      HOME으로 가기
+    </button>
+    <button onClick={()=>{
+      navigate(-1);
+    }}>뒤로가기</button>
     </>
   );
 };
