8962be7e91c352d9e538e306da6da752a1caa4b4
seoseongjo <tjtjdwh5321@gmail.com>
2022-11-27 23:36:18 +0900

    사이드 버튼 생성 및 코드 정리

--- diary/src/pages/Home.js
+++ diary/src/pages/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
 
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+  console.log(curDate);
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`
+
   return (
-    <>
-    <h1>Home</h1>
-    <p>이곳은 홈 입니다.</p>
-    </>
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={() => {}}/>}
+        rightChild={<MyButton text={">"} onClick={() => {}}/>}
+      />
+    </div>
   );
 };
 
