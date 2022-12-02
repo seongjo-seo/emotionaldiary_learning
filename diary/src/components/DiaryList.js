1c316e1ad6cbed7b42ca1c32a07e53992c128eb5
seoseongjo <tjtjdwh5321@gmail.com>
2022-12-02 18:28:45 +0900

    버튼 css 및 내비게이션 이동 방법 학습

--- diary/src/components/DiaryList.js
+++ diary/src/components/DiaryList.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import DiaryItem from "./DiaryItem";
 import MyButton from "./MyButton";
+import DiaryItem from "./DiaryItem";
 
 const sortOptionList = [
   {value : "lastest", name:"최신순"},
