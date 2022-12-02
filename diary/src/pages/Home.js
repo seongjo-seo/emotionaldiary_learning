fa96dd9db991232b1dcb51683f124c99afe0da5f
seoseongjo <tjtjdwh5321@gmail.com>
2022-12-02 16:14:49 +0900

    달력 이전 이후 일정 조정 기능 구현

--- diary/src/pages/Home.js
+++ diary/src/pages/Home.js
@@ -5,16 +5,26 @@ import MyButton from './../components/MyButton';
 
 const Home = () => {
   const [curDate, setCurDate] = useState(new Date());
-  console.log(curDate);
-
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`
+
+  const increaseMonth = () =>{
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () =>{
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
 
   return (
     <div>
       <MyHeader
         headText={headText}
-        leftChild={<MyButton text={"<"} onClick={() => {}}/>}
-        rightChild={<MyButton text={">"} onClick={() => {}}/>}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth}/>}
+        rightChild={<MyButton text={">"} onClick={increaseMonth}/>}
       />
     </div>
   );
