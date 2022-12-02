78fa80446cb10832f42125b9046b99e8331984eb
seoseongjo <tjtjdwh5321@gmail.com>
2022-12-02 17:27:18 +0900

    선택된 데이터만 불러오는 필터까지 구현.

--- diary/src/components/DiaryList.js
+++ diary/src/components/DiaryList.js
@@ -1,25 +1,46 @@
 import { useState } from "react";
 
 const sortOptionList = [
-  {value : "latest", name:"최신순"},
+  {value : "lastest", name:"최신순"},
   {value : "oldest", name:"오래된 순"},
-]
+];
+
+const filterOptionList = [
+  {value: "all", name:"전부다"},
+  {value: "good", name:"좋은 감정만"},
+  {value: "bad", name:"안좋은 감정만"},
+];
 
 
 /** 컨트롤 메뉴가 어떤 것을 선택하는지, 변화했을 때 바꿀 함수, 말 그대로 옵션  */
 const ControlMenu = ({value, onChange, optionList}) =>{
   return(
     <select value={value} onChange={(e)=>onChange(e.target.value)}>
-      {optionList.map((it, idx)=><option key={idx} value={it.value}>{it.name}</option>)}
+      {optionList.map((it, idx)=>(
+      <option key={idx} value={it.value}>
+        {it.name}
+        </option>
+        ))}
     </select>
   );
 };
 
 const DiaryList = ({diaryList})=>{
 
-  const [sortType, setSortType] = useState('lastest');
+  const [sortType, setSortType] = useState("lastest");
+  const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () =>{
+
+    const filterCallBack = (item)=>{
+      if(filter ==='good'){
+        return parseInt(item.emotion) <=3;
+      }else{
+        return parseInt(item.emotion) >3;
+      }
+    }
+
+
     const compare = (a, b)=>{
       if (sortType ==='lastest'){
         return parseInt(b.date) - parseInt(a.date);
@@ -30,7 +51,11 @@ const DiaryList = ({diaryList})=>{
     };
 
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+
+    const filterdList =
+      filter === 'all' ? copyList : copyList.filter((it)=>filterCallBack(it));
+
+    const sortedList = filterdList.sort(compare);
     return sortedList;
   }
 
@@ -41,11 +66,22 @@ const DiaryList = ({diaryList})=>{
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it)=>(
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );
+};
+
+DiaryList.defaultProps ={
+  diaryList: [],
 };
 
 export default DiaryList;
