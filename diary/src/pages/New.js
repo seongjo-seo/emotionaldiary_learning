75860a4dae1ad85b5ffaa71fc5a9463eaad729e9
seoseongjo <tjtjdwh5321@gmail.com>
2022-12-12 23:34:10 +0900

    컴포넌트 분리 및 css 정의

--- diary/src/pages/New.js
+++ diary/src/pages/New.js
@@ -1,41 +1,9 @@
-import React, {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import MyHeader from '../components/MyHeader';
-import MyButton from '../components/MyButton';
-
-const getStringDate = (date) =>{
-  return date.toISOString().slice(0,10);
-};
+import DiaryEditor from '../components/DiaryEditor';
 
 const New = () => {
-
-  console.log(getStringDate(new Date()));
-
-  const [date, setDate] =useState();
-
-  const navigate = useNavigate();
-
   return (
     <div>
-      <MyHeader
-        headText={"새 일기쓰기"}
-        leftChild={
-          <MyButton text={"< 뒤로가기"} onClick={()=>navigate(-1)}/>}
-      />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className='input-box'>
-            <input
-              className="input-date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              type="date"
-            />
-          </div>
-        </section>
-      </div>
+     <DiaryEditor/>
     </div>
   );
 };
